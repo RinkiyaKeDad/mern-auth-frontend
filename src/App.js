adca9e64eaf4e26fe177d1955f0bd5deef8fac02
RinkiyaKeDad <arshsharma461@gmail.com>
2020-07-18 14:55:38 +0530

    context added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Axios from 'axios';
+
 import Home from './components/pages/Home';
 import Header from './components/layout/Header';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import UserContext from './context/UserContext';
 import './style.css';
 
 export default function App() {
+  const [userData, setUserData] = useState({
+    token: undefined,
+    user: undefined,
+  });
+
+  //cant have an asynchronous funtion as our useEffect so we define a funtion inside the useEffect function
+  useEffect(() => {
+    const checkLoggedIn = async () => {
+      let token = localStorage.getItem('auth-token');
+
+      //this if is explained in Video 5 at 15:00
+      if (token === null) {
+        localStorage.setItem('auth-token', '');
+        token = '';
+      }
+      const tokenRes = await Axios.post(
+        'http://localhost:5000/users/tokenIsValid',
+        null, //null is for body data since we are not sending any data in the body
+        { headers: { 'x-auth-token': token } }
+      );
+      if (tokenRes.data) {
+        const userRes = await Axios.get('http://localhost:5000/users/', {
+          headers: { 'x-auth-token': token },
+        });
+        setUserData({
+          token,
+          user: userRes.data,
+        });
+      }
+    };
+
+    checkLoggedIn();
+  }, []);
+
   return (
     <>
       <BrowserRouter>
-        <Header />
-        <Switch>
-          <Route path='/' component={Home} exact />
-          <Route path='/login' component={Login} />
-          <Route path='/register' component={Register} />
-        </Switch>
+        <UserContext.Provider value={{ userData, setUserData }}>
+          <Header />
+          <Switch>
+            <Route path='/' component={Home} exact />
+            <Route path='/login' component={Login} />
+            <Route path='/register' component={Register} />
+          </Switch>
+        </UserContext.Provider>
       </BrowserRouter>
     </>
   );
